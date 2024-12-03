b0f16a7ee08c74487947140089ba4feb1b766655
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-12-03 16:41:02 -0300

    saimon

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma linguagem de programação?',
+    'Uma linguagem de programação é um sistema de comunicação entre o programador e o computador.'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde está localizada a cidade de Machu Picchu?',
+    'Machu Picchu está localizada no Peru, nas montanhas dos Andes.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a diferença entre uma lista e uma tupla em Python?',
+    'A principal diferença é que as listas são mutáveis, enquanto as tuplas são imutáveis.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "obrigado" em inglês?',
+    'Obrigado em inglês é "thank you".'
+)
